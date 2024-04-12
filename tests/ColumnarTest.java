f24c55d333d17a526a97a84ef6c2b3b1612d96fa
Omkar Shelar <oshelar2@asu.edu>
2024-04-11 19:33:15 -0700

    Cleanup: Remove class files
    - Add some more tests to columnar store

--- tests/ColumnarTest.java
+++ tests/ColumnarTest.java
@@ -7,6 +7,7 @@ import columnar.ColumnarFiles;
 import global.AttrType;
 import global.GlobalConst;
 import global.SystemDefs;
+import heap.Heapfile;
 
 public class ColumnarTest extends TestDriver implements GlobalConst {
 
@@ -23,15 +24,21 @@ public class ColumnarTest extends TestDriver implements GlobalConst {
     public static void main(String[] args) {
         ColumnarTest colFileTest = new ColumnarTest();
         if (!colFileTest.test1()) {
-            System.out.println(ANSI_RED + "ColumnFileTest.test1() failed" + ANSI_RESET);
+            System.out.println(ANSI_RED + "ColumnarTest.test1() failed" + ANSI_RESET);
             System.exit(1);
         }
-        System.out.println(ANSI_GREEN + "ColumnFileTest passed" + ANSI_RESET);
+        System.out.println(ANSI_GREEN + "ColumnarTest.test1() passed" + ANSI_RESET);
+        if (!colFileTest.test2()) {
+            System.out.println(ANSI_RED + "ColumnarTest.test2() failed" + ANSI_RESET);
+            System.exit(1);
+        }
+        System.out.println(ANSI_GREEN + "ColumnarTest.test2() passed" + ANSI_RESET);
+        System.out.println(ANSI_GREEN + "🎉 All ColumnarTest tests passed! 🎉" + ANSI_RESET);
     }
 
     public ColumnarTest() {
-        super("ColumnFileTest");
-        System.out.print("\n" + "Running ColumnFileTest tests...." + "\n");
+        super("ColumnarTest");
+        System.out.print("\n" + "Running ColumnarTest...." + "\n");
 
         try {
             SystemDefs sysdef = new SystemDefs(dbpath, NUMBUF + 20, NUMBUF, "Clock");
@@ -80,6 +87,8 @@ public class ColumnarTest extends TestDriver implements GlobalConst {
     }
 
     protected boolean test1() {
+        System.out.println(
+                "Running Test 1: Create a new ColumnFile, setting some values, copying it using getBytes() and checking it with original data");
         String columnFileName = "Test_Column_File_1.col";
         String columnName = "Test Column Int";
         AttrType attrType = new AttrType(1);
@@ -91,22 +100,71 @@ public class ColumnarTest extends TestDriver implements GlobalConst {
 
         if (!columnFileCheck.getName().equals(columnFileName)) {
             return false;
-        } else if (!columnFileCheck.getColumnName().equals(columnName)) {
-            return false;
-        } else if (!columnFileCheck.getAttrType().toString().equals(attrType.toString())) {
-            return false;
-        } else if (columnFileCheck.hasBtree()) {
-            return false;
-        } else if (!columnFileCheck.hasBitmap()) { // should be true set above
+        }
+        if (!columnFileCheck.getColumnName().equals(columnName)) {
             return false;
         }
-        System.out.println(columnFileCheck.getFile());
+        if (!columnFileCheck.getAttrType().toString().equals(attrType.toString())) {
+            return false;
+        }
+        if (columnFileCheck.hasBtree()) {
+            return false;
+        }
+        if (!columnFileCheck.hasBitmap()) { // should be true set above
+            return false;
+        }
         return true;
     }
 
-    // protected boolean test2() {
-    //     String[] colNames = {"Test Col 1", "Test Col 2", "Test Col 3", "Test Col 4"};
-    //     AttrType[] colAttrs = {new AttrType(AttrType.attrInteger), new AttrType(AttrType.attrString), new AttrType(AttrType.attrReal), new AttrType(AttrType.attrString)};
-    //     ColumnarFiles columnarFiles = new ColumnarFiles("ColumarFilesTest", colNames, colAttrs, colNames.length);
-    // }
+    protected boolean test2() {
+        String[] colNames = { "Test Col 1", "Test Col 2", "Test Col 3", "Test Col 4" };
+        AttrType[] colAttrs = { new AttrType(AttrType.attrInteger), new AttrType(AttrType.attrString),
+                new AttrType(AttrType.attrReal), new AttrType(AttrType.attrString) };
+        ColumnarFiles columnarFiles = new ColumnarFiles("ColumarFilesTest", colNames, colAttrs, colNames.length);
+
+        ColumnFile testCol2 = columnarFiles.getColumnFile(1);
+        testCol2.setHasBtree(true);
+
+        ColumnarFiles columnarFilesCheck = new ColumnarFiles("ColumarFilesTest");
+
+        testCol2 = columnarFilesCheck.getColumnFile(1);
+
+        if (columnarFilesCheck.getNumColumns() != 4) {
+            return false;
+        }
+        try {
+            // This should fail
+            columnarFilesCheck.getColumnFile(5);
+            return false;
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Getting a non-existent column fails as expected!");
+        }
+
+        try {
+            columnarFilesCheck.getTidFile();
+        } catch (Exception e) {
+            return false;
+        }
+
+        try {
+            Heapfile headerFile = columnarFilesCheck.getHeaderFile();
+            if (headerFile.getRecCnt() != 4) {
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace(); // oops!
+        }
+
+        if (testCol2.getAttrType().toString() != new AttrType(0).toString()) {
+            System.out.println("testCol2 attrType compare failed");
+            return false;
+        }
+
+        if(!testCol2.getColumnName().equals("Test Col 2")) {
+            System.out.println("testCol2.getColumnName() failed");
+            return false;
+        }
+
+        return true;
+    }
 }
